240a2e21ffe5daafff1f76559b1c9bf39edf91ac
colespehar <cole.spehar97@gmail.com>
2020-12-07 11:22:19 -0500

    Update

---
+++ ML04-WPF/Email.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ML04_WPF
+{
+    /// <summary>
+    /// Interaction logic for Email.xaml
+    /// </summary>
+    public partial class Email : Window
+    {
+        public Email()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
